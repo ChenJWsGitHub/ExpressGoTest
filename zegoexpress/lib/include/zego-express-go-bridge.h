f6979f85e9842131ab2e549e6ed6d795ed4aec02
javion <javionchen@zego.im>
2025-09-30 17:40:34 +0800

    delete  libZegoExpressGoBridge.so

--- zegoexpress/lib/include/zego-express-go-bridge.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __ZEGO_EXPRESS_ENGINE_GO_BRIDGE_H__
-#define __ZEGO_EXPRESS_ENGINE_GO_BRIDGE_H__
-
-#include "zego-express-defines.h"
-
-ZEGOEXP_API void EXP_CALL zego_express_go_bridge_init();
-
-#endif
