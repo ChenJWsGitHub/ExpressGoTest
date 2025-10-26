afb224527611528928f74be2d6d3679548a05fbb
javion <javionchen@zego.im>
2025-10-26 17:54:06 +0800

    Supports multiple architectures

---
+++ zegoexpress/ZegoExpressEngineImpl_linux_amd64.go
@@ -0,0 +1,15 @@
+//go:build linux && amd64
+package zegoexpress
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/lib/include
+#cgo LDFLAGS: -L${SRCDIR}/lib/linux-amd64 -lZegoExpressEngine -Wl,-rpath=${SRCDIR}/lib/linux-amd64
+#include "zego-express-engine.h"
+*/
+import "C"
+
+func getVersion() string {
+	var cVersion *C.char
+	C.zego_express_get_version(&cVersion)
+	return C.GoString(cVersion)
+}
